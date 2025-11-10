d4a6cc6547e34d49c3e6f703b6ec8c91e0277783
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-10 23:09:23 +0100

    Remove remove trigger drop

--- cmd/myawesomelist/app/migrations/000001_create_collections_table.down.sql
+++ cmd/myawesomelist/app/migrations/000001_create_collections_table.down.sql
@@ -1,9 +1,6 @@
 -- Drop trigger before dropping its function/table
 DROP TRIGGER IF EXISTS update_collections_updated_at ON collections;
 
--- Remove the helper function used by the trigger
-DROP FUNCTION IF EXISTS update_updated_at_column();
-
 -- Explicitly drop indexes (also dropped with table, but clearer here)
 DROP INDEX IF EXISTS idx_collections_hostname_owner_repo;
 DROP INDEX IF EXISTS idx_collections_updated_at;
