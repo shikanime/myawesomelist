b7843e0bcf2128bf29f73e51fb0a86311545174a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-10 22:46:23 +0100

    Add API url build arg

--- www/app/api/client.ts
+++ www/app/api/client.ts
@@ -2,7 +2,7 @@ import { createConnectTransport } from "@connectrpc/connect-web";
 import { createClient } from "@connectrpc/connect";
 import { AwesomeService } from "~/proto/myawesomelist/v1/myawesomelist_pb";
 
-const baseUrl = import.meta.env.MYAWESOMELIST_BASE_URL ?? "http://localhost:8080";
+const baseUrl = import.meta.env.VITE_MYAWESOMELIST_BASE_URL ?? "http://localhost:8080";
 
 const transport = createConnectTransport({
   baseUrl,
