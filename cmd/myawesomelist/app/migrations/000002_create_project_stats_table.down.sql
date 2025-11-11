01eff6c25eab6a366795311dc8d37d7559fcf2fa
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-11 05:01:39 +0100

    Migrate to gorm
    ghstack-source-id: 66a78b05c94f334f60c94092f9a9de2ee7649063
    Pull Request resolved: https://github.com/shikanime/myawesomelist/pull/43

--- cmd/myawesomelist/app/migrations/000002_create_project_stats_table.down.sql
+++
@@ -1,9 +0,0 @@
--- Drop trigger before dropping its function/table
-DROP TRIGGER IF EXISTS update_project_stats_updated_at ON project_stats;
-
--- Explicitly drop indexes (also dropped with table, but clearer here)
-DROP INDEX IF EXISTS idx_project_stats_hostname_owner_repo;
-DROP INDEX IF EXISTS idx_project_stats_updated_at;
-
--- Drop table
-DROP TABLE IF EXISTS project_stats;
--- internal/sqlx/upsert_collection.sql.tpl
+++
@@ -1,7 +0,0 @@
-INSERT INTO collections (hostname, owner, repo, language)
-VALUES ($1, $2, $3, $4)
-ON CONFLICT (hostname, owner, repo)
-DO UPDATE SET
-    language = EXCLUDED.language,
-    updated_at = NOW()
-RETURNING id
